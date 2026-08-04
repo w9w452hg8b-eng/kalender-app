5ec91224f2381600c32a843d6e6c0de8d64ee8e7
w9w452hg8b-eng <w9w452hg8b@privaterelay.appleid.com>
2026-08-05 00:52:18 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,3 +6,17 @@ self.addEventListener('fetch', function(event) {
     })
   );
 });
+self.addEventListener('push', function(event) {
+  const data = event.data ? event.data.json() : {};
+  const title = data.title || 'Erinnerung';
+  const options = {
+    body: data.body || 'Ein Kalender-Termin steht an!',
+    icon: '/playstore.png',
+    badge: '/playstore.png',
+    vibrate: [200, 100, 200]
+  };
+
+  event.waitUntil(
+    self.registration.showNotification(title, options)
+  );
+});
